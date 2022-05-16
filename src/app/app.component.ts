f3337a1a511bb4b400e3468c08653a526ea803cc
wabyroadefgcu <wabyroade8918@eagle.fgcu.edu>
2022-05-15 20:05:33 -0400

    Modify netlifyIdentityService to check user role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NetlifyIdentityService } from './netlify-identity.service';
 
 @Component({
@@ -6,7 +6,73 @@ import { NetlifyIdentityService } from './netlify-identity.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'invoicing';
+
   constructor(public ns: NetlifyIdentityService) { }
+
+  ngOnInit(): void {
+    // if (this.ns.checkUser()) {
+    //   console.log("logged in, populating userEmail");
+    //   this.ns.populateUserData();
+    // }
+    console.log(`isAdmin: ${this.ns.isAdmin}`);
+    console.log(`loggedIn: ${this.ns.loggedIn}`);
+    console.log(`userEmail: ${this.ns.userEmail}`);
+
+    console.log("1")
+    console.log(this.ns)
+    console.log("2")
+    console.log(this.ns.get())
+//     console.log("3")
+//     console.log(this.us)
+// //    console.log(ns.get().currentUser())
+//     console.log("4")
+//     console.log(this.us.getWhatever())
+// //    console.log(ns.get().currentUser().email)
+//     console.log("5")
+//     console.log(this.us.getWhatever2())
+// //    console.log(this.ns.getUser())
+//     console.log("6")
+//     console.log(this.us.someValue)
+//     console.log("7")
+//     console.log(this.ns.checkUser())
+//     if (this.ns.get().currentUser() != null) {
+//       var thisUser = this.ns.get().currentUser();
+
+//       console.log(thisUser);
+//       console.log(thisUser.app_metadata);
+
+// //      console.log(thisUser.app_metadata.roles);
+//       var theseRoles: string[] = thisUser.app_metadata.roles;
+//       console.log(theseRoles);
+//       theseRoles.forEach((element) => {
+//         console.log(element);
+//       });
+
+//       var isAdmin = theseRoles.indexOf("Administrator");
+//       console.log("isAdmin: ");
+//       console.log(isAdmin);
+
+//       var isAdmin2 = theseRoles.find((value) => {
+//         return value === "Administrator"
+//       })
+//       console.log("isAdmin2: ");
+//       console.log(isAdmin2);
+
+//       var isAdmin3 = theseRoles.includes("Administrator");
+//       console.log("isAdmin3: ");
+//       console.log(isAdmin3);
+//       // this.ns.get().currentUser().app_metadata.roles.array.foreach((element: any) => {
+//       //   console.log(element);
+//       // });
+//     }
+
+
+//    console.log(this.ns.getUserStatus())
+    // ns.get().currentUser() 
+    // ns.get().currentUser().email
+
+  }
 }
--- src/app/netlify-identity.service.ts
+++ src/app/netlify-identity.service.ts
@@ -8,38 +8,74 @@ declare var netlifyIdentity: any;
 })
 
 export class NetlifyIdentityService {
-    constructor(router: Router) {
-      netlifyIdentity.init();
-      // Bind to events
-      netlifyIdentity.on('init', function(user: any) {
-        console.log('init', user)
-      });
 
-      netlifyIdentity.on('login', function(user: any) {   
-        console.log('current user', netlifyIdentity.currentUser());
-        netlifyIdentity.close();
-      });
+  userEmail: string = "";
+  loggedIn: boolean = false;
+  isAdmin = this.runOnCheck();
 
-      netlifyIdentity.on('logout', function() {
-        console.log('Logged out'); 
-        netlifyIdentity.close();
-        router.navigateByUrl('/');
-      });
-
-      netlifyIdentity.on('error', function(err: any) {
-        console.error('Error', err)
-      }); 
-
-      netlifyIdentity.on('open', function() {
-        console.log('Widget opened')
-      });
-
-      netlifyIdentity.on('close', function() {
-        console.log('Widget closed');
-      });
+  private runOnCheck(): boolean {
+    if (netlifyIdentity.currentUser()) {
+      this.userEmail = netlifyIdentity.currentUser().email;
+      return true;
     }
-    
-    get(): any {
-        return netlifyIdentity;
+    return false;
+  }
+
+  constructor(router: Router) {
+    netlifyIdentity.init();
+    // Bind to events
+    netlifyIdentity.on('init', function(user: any) {
+      console.log('init', user)
+    });
+
+    netlifyIdentity.on('login', (user: any) => {   
+      console.log('current user', netlifyIdentity.currentUser());
+      this.userEmail = netlifyIdentity.currentUser().email;
+      console.log('current user alt', user);
+      netlifyIdentity.close();
+    });
+
+    netlifyIdentity.on('logout', function() {
+      console.log('Logged out'); 
+      netlifyIdentity.close();
+      router.navigateByUrl('/');
+    });
+
+    netlifyIdentity.on('error', function(err: any) {
+      console.error('Error', err)
+    }); 
+
+    netlifyIdentity.on('open', function() {
+      console.log('Widget opened')
+    });
+
+    netlifyIdentity.on('close', function() {
+      console.log('Widget closed');
+    });
+
+    let initialUser = netlifyIdentity.currentUser();
+    if (initialUser) {
+      this.userEmail = initialUser.email;
+      this.loggedIn = true;
+    }
+
+  }
+  
+  get(): any {
+      return netlifyIdentity;
+  }
+
+  checkUser(): boolean {
+    var tempValue = netlifyIdentity.currentUser();
+    if (tempValue !== null) {
+      return true;
+    }
+    return false;
+  }
+
+  populateUserData(): void {
+    if (netlifyIdentity.currentUser()) {
+      this.userEmail = netlifyIdentity.currentUser().email;
     }
   }
+}
